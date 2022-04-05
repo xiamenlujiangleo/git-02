4f752dcdf4bbe507aec27ecb2ae8751d4ac6976a
leo <leo@java.com>
2022-04-05 15:47:49 +0800

    push test

--- src/main/java/com/cfx/GitTest.java
+++ src/main/java/com/cfx/GitTest.java
@@ -7,6 +7,7 @@ public class GitTest {
         System.out.println("hello hot-fix");
         System.out.println("hot-fix test");
         System.out.println("hot-fix modified");
+        System.out.println("push test");
 
     }
 }
