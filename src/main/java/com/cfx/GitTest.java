891d9058cd6575f3ab0df2b13f4a7abfa61e47de
leo <leo@java.com>
2022-04-05 15:16:00 +0800

    hot-fix commit01

--- src/main/java/com/cfx/GitTest.java
+++ src/main/java/com/cfx/GitTest.java
@@ -4,5 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello GIT");
         System.out.println("hello GIT2");
+        System.out.println("hello hot-fix");
+
     }
 }
